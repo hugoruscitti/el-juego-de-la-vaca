13f2c524a43b7c157671cf9e4b4adaa1eaf3291f
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-11-11 17:53:40 -0300

    update

---
+++ Assets/startGameScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startGameScript : MonoBehaviour {
+	public GameObject camera;
+	public GameObject playerController;
+
+	void Start () {
+		playerController.SendMessage("startRunning");
+		camera.SendMessage("startFollowCow");
+	}
+
+}
