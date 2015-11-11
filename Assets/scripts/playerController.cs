57763c9750e6ff48c210ccd47bff571a08033382
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-11-11 17:43:55 -0300

    update

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -8,14 +8,15 @@ public class playerController : MonoBehaviour {
 	public GameObject target;
 	private Vector3 advance;
 	public float runningSpeed;
+	public float reduceHorizontalSpeedFactor;
 
 	void Start() {
 		advance = new Vector3(1, 0, 0);
 	}
 
 	void Update () {
-		Vector3 movementKeyboard = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		Vector3 movementJoystick = new Vector3(CnInputManager.GetAxis("Horizontal"), 0, CnInputManager.GetAxis("Vertical"));
+		Vector3 movementKeyboard = new Vector3(Input.GetAxis("Horizontal") * reduceHorizontalSpeedFactor, 0, Input.GetAxis("Vertical"));
+		Vector3 movementJoystick = new Vector3(CnInputManager.GetAxis("Horizontal") * reduceHorizontalSpeedFactor, 0, CnInputManager.GetAxis("Vertical"));
 
 		direction = (movementKeyboard + movementJoystick) * speed;
 	}
