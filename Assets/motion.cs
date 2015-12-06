144d26c1d72b6540feda267d97e21726bd157b57
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-12-06 15:22:00 -0300

    update

---
+++ Assets/motion.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class motion : MonoBehaviour {
+	public float speed;
+
+	void Update () {
+		transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+
+		if (transform.position.x < -80) {
+			transform.position = new Vector3(58, transform.position.y, transform.position.z);
+		}
+	}
+}
